2eecc5f69a058c429bf4687a5aec839073f7e631
alanranger <info@alanranger.com>
2025-11-02 13:37:28 +0000

    Add missing folders with .gitkeep for GitHub visibility

---
+++ scripts/reviews-squarespace.js
@@ -0,0 +1,338 @@
+export default [
+  {
+    reviewer: 'Graham Rippon',
+    body: 'A useful, well-produced publication. Delivered quickly.',
+    rating: 5,
+    date: 'Sep 6, 2022',
+    event: 'Top 10 Tips',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'David Albone',
+    body: 'Ok but took ages for order to arrive. Was on the point of complaining!',
+    rating: 3,
+    date: 'Jul 19, 2023',
+    event: 'Top 10 Tips',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Kevin Atkins',
+    body: "Alan is very knowledgeable and was able to quickly give feedback on my photos and also give me information about the LRPS assessment process which wasn't on their website. I look forward to our next session.",
+    rating: 5,
+    date: 'Jan 5, 2025',
+    event: 'Private Tuition',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Michael Simm',
+    body: 'Purchased this as it’s easier than clicking through the online course and as a way of donating for the free course. Course is exactly what I was looking for and the course book is excellently written and a great reference. All the authoring effort much appreciated Alan - thank you!!',
+    rating: 5,
+    date: 'Jul 14, 2025',
+    event: 'Alan Ranger Academy',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Jeremy Troth',
+    body: 'Excellently organised. Always Alan is prepared to give you the full benefit of his skills and knowledge, which is extensive. His criticism of your photographs is constructive and I benefited from it greatly. I would thoroughly recommend him.',
+    rating: 5,
+    date: 'Jun 7, 2022',
+    event: 'Anglesely Photography Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'JANE BERRISFORD',
+    body: 'A great workshop in a wonderful area with so many photographic opportunities. Alan was very helpful with making sure that I got the right composition for the best images. Would recommend !',
+    rating: 5,
+    date: 'May 13, 2022',
+    event: 'Anglesely Photography Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Derek Finnigan',
+    body: 'Excellent tour with great direction from Alan, the skill of knowing where to take a group is critical for these tours. Alan quickly understood our goals and helped us as needed to get what we were looking for. Easy going and there when needed.',
+    rating: 5,
+    date: 'Jun 19, 2023',
+    event: 'Anglesely Photography Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'David Pitt',
+    body: 'Another great event from Alan. Always very helpful with his advice. Batsford is a great place for autumn photography.',
+    rating: 5,
+    date: 'Nov 17, 2022',
+    event: 'Batsford Autumn Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Elaine Cash',
+    body: 'I joined Alan & the group for a half day workshop and was instantly made very welcome. We managed a good walk and covered various trees, foliage & flowers and although unfortunately it wasn’t a particularly sunny day, Alan gave fabulous advice on light and composition. Really great workshop which I would highly recommend anyone to take!',
+    rating: 5,
+    date: 'May 15, 2022',
+    event: 'Batsford Autumn Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Sara Taylor',
+    body: 'Alan was very friendly and knowledgeable - happy to answer the many and varied questions the group asked him',
+    rating: 5,
+    date: 'Nov 6, 2023',
+    event: 'Batsford Autumn Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Martin Vincent',
+    body: 'Great leader much better than many workshop I have attended',
+    rating: 5,
+    date: 'Nov 6, 2023',
+    event: 'Batsford Autumn Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Ann Hume',
+    body: 'I really enjoyed my beginners course. It inspired me to use more of my cameras functions and be more creative. It is clear Alan is a very talented photographer but he is also a good communicator and teacher. I hope to put my learning into practice and go on more courses with Alan.',
+    rating: 5,
+    date: 'Feb 27, 2023',
+    event: 'Beginners Photography Course',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Nicholas Barber',
+    body: 'Thoroughly enjoyable experience and a great way to improve my photography. The wood is amazing and extensive. Alan is on hand throughout to make suggestions and give advice. I got some beautiful images.',
+    rating: 5,
+    date: 'Jun 16, 2022',
+    event: 'Bluebell Woodlands Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'sue green',
+    body: 'really enjoyable event. Learned a lot',
+    rating: 5,
+    date: 'May 20, 2022',
+    event: 'Bluebell Woodlands Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Dan Horrocks',
+    body: 'Perfect bluebell photoshoot. And a very friendly and knowledgeable photographer.',
+    rating: 5,
+    date: 'May 12, 2022',
+    event: 'Bluebell Woodlands Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'John Madigan',
+    body: 'Good day with like minded people. Good spot to TRY to take bluebell pictures. Good advice from Alan. However what you see with the eye is really difficult to capture through to print. Must try again next year.',
+    rating: 5,
+    date: 'May 15, 2024',
+    event: 'Bluebell Woodlands Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Kurt Mueller',
+    body: 'I signed on for 2 workshops on the same day and enjoyed both of them. The wonderful private woods provided a perfect setting for a variety of images. Alan provided whatever help was needed. walking through the unspoilt bluebells along the paths was a special experience on its own! Highly recommended. Kurt, Switzerland',
+    rating: 5,
+    date: 'May 13, 2024',
+    event: 'Bluebell Woodlands Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Ian Hope',
+    body: 'Photo workshop to private bluebell woods. Alan gave a useful briefing on some key technical pointers and suggestions on composition. Always on hand to offer further guidance.',
+    rating: 4,
+    date: 'May 12, 2024',
+    event: 'Bluebell Woodlands Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Peter Pauwels',
+    body: "This is my second review: apologies if it isn't the same as the first! I thought this was a very good event in what was probably the most extensive bluebell wood I've ever visited. We were blessed with excellent weather which was a great advantage. It was also a huge advantage to have exclusive access to the wood because we didn't have to cope with members of the public wandering into our shots; also the bluebells hadn't been downtrodden. My purpose in joining was to gain access to the location which from images on the website looked (and proved to be exceptional) but for those who needed it Alan was on hand to offer help and advice. We were confined to a narrow path which occasionally made it difficult to pass other members of the group but there were only 4 of us so it wasn't much of an issue. The main problem was not knowing in advance just how extensive the woods were. This made it very tricky to time ourselves and Alan applied some pressure after the 1st hour by saying we hadn't seen a tenth of what was on offer. The net result was that I spent too long at the beginning and had to rush through at the end missing many great shots. I do not know how to solve this problem for those attending for the first time: perhaps the answer is to return next year!",
+    rating: 4,
+    date: 'May 11, 2022',
+    event: 'Bluebell Woodlands Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'David Pitt',
+    body: 'Great session of photography in the bluebell woods. Help and advice from Alan very much appreciated.',
+    rating: 4,
+    date: 'May 11, 2022',
+    event: 'Bluebell Woodlands Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Rosamund Sheppard',
+    body: 'A beautiful calendar I hope to look out for another next year.',
+    rating: 5,
+    date: 'Apr 30, 2022',
+    event: 'Calendar',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Linda Newbery',
+    body: 'I really enjoyed the evening. Night photography was completely new to me but I learned a lot from Alan, gained confidence and was pleased with some of my images. The small group meant that there was plenty of one-to-one tuition. Now to get out and practise more!',
+    rating: 5,
+    date: 'Dec 23, 2022',
+    event: 'Christmas Photography Seasonal Workshops | Warwickshire',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Philippa King',
+    body: 'Excellent workshop. This was my second residential workshop with Alan. As usual he was approachable, knowledgable and helpful. The organisation was very good. My individual needs were catered for too.',
+    rating: 5,
+    date: 'Apr 29, 2024',
+    event: 'Doreset Photography Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Fiona Clifford',
+    body: 'Well organised course, great locations. Alan was a knowledgeable host with patience and flexibility. Looking forward to attending more workshops.',
+    rating: 5,
+    date: 'Apr 2, 2022',
+    event: 'Doreset Photography Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Derek Finnigan',
+    body: 'Unfortunately the locations are very water level dependant and could have done with more water. However Alan still made it interesting and very helpful with advice. Happy to share his skill and experience',
+    rating: 4,
+    date: 'Jun 19, 2023',
+    event: 'Fairy Glen Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Margit Claudia Dr. Dr. Kollenda',
+    body: 'Pleasant atmosphere, professionell criticsm, I learned a lot, very indivduell teaching. I can recommend it wholeheartedly',
+    rating: 5,
+    date: 'Dec 6, 2022',
+    event: 'Lake District Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Martin Lancaster',
+    body: "Had a great workshop experience with Alan in the Lake District. He's very knowledgeable, practical and unpretentious in his approach. You can learn from him whatever your experience level. The image reviews at the end of each day were invaluable. I learned a lot in just a few days and came away inspired. I wouldn't hesitate to book another workshop in future.",
+    rating: 5,
+    date: 'Apr 7, 2024',
+    event: 'Lake District Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'David Andringa',
+    body: 'Alan must have spent ages researching the vistas that we went to. There was a good mix of different places to shoot. Alan took time to answer questions and even showed us some shots that he had taken to try and get another perspective. Thoroughly recommend!',
+    rating: 5,
+    date: 'Apr 9, 2022',
+    event: 'Lake District Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Nicholas Barber',
+    body: 'An excellent and full on weekend in which we visited a range of locations/subjects and took some great shots. Advice was always on hand and we all learned a lot. Good fun too!!',
+    rating: 5,
+    date: 'Sep 26, 2022',
+    event: 'Norfolk Coastal Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'David Pitt',
+    body: 'Really enjoyed the Lightroom Classic course with Alan. It was well structured over the 4 evenings and I learned a lot about the software structure and editing capabilities.',
+    rating: 5,
+    date: 'Apr 3, 2024',
+    event: 'Lightroom Course',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'David Pitt',
+    body: "Great workshop for getting know the camera better with Alan's help and guidance. Got some great photos. Recommend this course.",
+    rating: 5,
+    date: 'Jun 29, 2022',
+    event: 'Long Exposure Photography Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Sara Orsbourn',
+    body: 'As always a fabulous workshop. Gave great advice on composition. Will be back for another workshop',
+    rating: 5,
+    date: 'May 2, 2023',
+    event: 'Abstract and Macro Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'adrian thomas',
+    body: "Alan has a wealth of photography knowledge and experience. But crucially he's a really good guy who will willingly help directly, or hold back as required. No egos and has always made me feel comfortable. I'll be back.",
+    rating: 5,
+    date: 'Oct 16, 2024',
+    event: 'Garden Photography Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Linda Newbery',
+    body: 'Lovely venue and good tuition in a group of just 4 (including Alan). Enjoyable and informative.',
+    rating: 5,
+    date: 'Sep 12, 2023',
+    event: 'Garden Photography Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Gabriela Goncalves',
+    body: 'Great weekend, I definitely learned a lot that I will be using in future. Would happily book another workshop with Alan.',
+    rating: 5,
+    date: 'Apr 18, 2023',
+    event: 'Snowdonia Photography Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'mark godfrey',
+    body: 'It was a good afternoon. Always come away with something useful. Many thanks.',
+    rating: 5,
+    date: 'Sep 6, 2022',
+    event: 'Garden Photography Workshop',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Mandy Watts',
+    body: 'Customer service has been exceptional. Alan gave me great advice to purchase a new camera and was very understanding when a family emergency got in the way of my workshop.',
+    rating: 5,
+    date: 'Jan 9, 2025',
+    event: 'The Secret of Woodland Photography',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Kirsten Pearce',
+    body: 'Enjoyed spending a day concentrating just on woodland photography during the woodland photography masterclass. The day provided a good mix of theory, practical and post processing and the small group allowed everyone to benefit from Alan’s expertise. Would recommend this day, thanks Alan.',
+    rating: 5,
+    date: 'Dec 11, 2023',
+    event: 'The Secret of Woodland Photography',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Lynn Rushall',
+    body: "The Secrets of Woodland Photography Workshop was the first of Alan's courses that I have attended. I greatly enjoyed the workshop, the small informal group, and the relaxed style of presentation. The blend of introductory theory, progressing logically to recommended/advisory camera settings; and followed by a practical session in the adjacent woodland worked well. After a refreshment break, attention turned to post production analysis of the morning woodland session. Alan was at pains to ensure that the work of everyone was given equal consideration, and we all received a copy of the suggested improvements made to our images for later review. Overall I was pleased with my first experience of Alan Ranger Photography, although my personal preparation for this workshop fell short of the ideal. I found the sessions informative and enjoyable. Alan's style encouraged participation from delegates, all of whom I found intelligent and engaging, and very willing to share their knowledge and experience, which was very helpful to me as a newcomer. I am already planning for my next workshop.",
+    rating: 4,
+    date: 'Oct 31, 2023',
+    event: 'The Secret of Woodland Photography',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Nick Barber',
+    body: 'Excellent. An unusual and well designed day. Woodland photography is difficult and the design of the day was perfect - photography in the morning and a general session on how to process the images in the afternoon - this included processing our images and getting presets to help with our own processing. Detailed theory talk and handout.',
+    rating: 5,
+    date: 'Oct 30, 2023',
+    event: 'The Secret of Woodland Photography',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Michael Mackay',
+    body: "I had lost my confidence in handling my DSLR, BUT Alan's workshop re-inspired me. His exercise and guidance helped me to master the main controls. First rate workshop.",
+    rating: 5,
+    date: 'Oct 16, 2023',
+    event: 'The Secret of Woodland Photography',
+    source: 'Squarespace'
+  },
+  {
+    reviewer: 'Bindu Felix',
+    body: "I recently attended my first walk with a group of photographers, which was organized by Alan. I wanted to express my appreciation for Alan's extensive experience as an interactive photographer. He skilfully kept the group engaged through discussions and practical exercises, allowing for valuable knowledge sharing. Overall, the two-hour walk was a great investment for someone like me who is just starting out in photography. I wish to attend his extensive residential trip some day soon.",
+    rating: 5,
+    date: 'Nov 22, 2023',
+    event: 'Woodland Photowalk',
+    source: 'Squarespace'
+  },
+];
