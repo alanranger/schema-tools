76e00ac7f8694cfcdea25e618fd386a362903184
alanranger <info@alanranger.com>
2025-11-06 14:28:11 +0000

    fix: correct fs import in main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import { app, BrowserWindow, ipcMain, shell } from "electron";
 import path from "path";
 import { spawn } from "child_process";
 import { fileURLToPath } from "url";
+import fs from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
